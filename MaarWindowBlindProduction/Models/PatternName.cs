4669f47687c8a64d52ef22c563a47fb9cb635a76
Hendris <hendris.maar@gmail.com>
2022-11-22 15:24:19 +0200

    Add new field to PatternName model

--- MaarWindowBlindProduction/Models/PatternName.cs
+++ MaarWindowBlindProduction/Models/PatternName.cs
@@ -5,5 +5,6 @@
         //pattern names: https://www.designerblogs.com/background-patterns-guide/
         public int Id { get; set; }
         public string Pattern { get; set; }
+        public int PatternId { get; set; }
     }
 }
